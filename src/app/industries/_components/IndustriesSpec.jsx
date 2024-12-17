213fd1370eb748e4f9dbb85f789f40ef07e90b57
KonstantinRTS <k.developer@acceptance.ltd>
2024-12-17 23:05:32 +0200

    all files 24

--- src/app/industries/_components/IndustriesSpec.jsx
+++ src/app/industries/_components/IndustriesSpec.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import "@/styles/industries.scss";
 import { fadeInUp } from "@/utils/animations";
 import { motion } from "framer-motion";
+import IndustriesSlider from "./IndustriesSlider";
 
 const IndustriesSpec = () => {
     return (
@@ -20,7 +21,9 @@ const IndustriesSpec = () => {
                             <h2 className="indusctries-spec__title">Specialised Expertise Across Key Sectors</h2>
                             <div className="indusctries-spec__text">We specialise in delivering results across a variety of industries. Below is a snapshot of the sectors we serve, along with examples of how we make an impact:</div>
                         </motion.div>
-                        <div className="indusctries-spec__slider"></div>
+                        <div className="indusctries-spec__slider">
+                            <IndustriesSlider />
+                        </div>
                     </div>
                 </div>
             </section>
