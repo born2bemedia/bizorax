77547165733b0111540581827a8f55be9c8bb12b
KonstantinRTS <k.developer@acceptance.ltd>
2024-12-18 14:44:59 +0200

    all files 35

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
+import "@/styles/base.scss";
 
 export default function NotFound() {
     return (
-        <div className="404">
-            <div className="404__container _container">
-                <div className="404__body">
-                    <h1 className="404__title">404</h1>
-                    <h2 className="404__subtitle">Page not found</h2>
-                    <p className="404__text">Oh, we can’t seem to find the page you’re looking for. Try going back to the previous page or:</p>
-                    <Link href="/" className="404__link">Go to the home page</Link>
+        <div className="error">
+            <div className="error__container _container">
+                <div className="error__body">
+                    <h1 className="error__title">404</h1>
+                    <h2 className="error__subtitle">Page not found</h2>
+                    <p className="error__text">Oh, we can’t seem to find the page you’re looking for. Try going back to the previous page or:</p>
+                    <Link href="/" className="error__link">Go to the home page</Link>
                 </div>
             </div>
+            <div className="error__image"><img src="/images/contact/img-01.jpg" alt="image" /></div>
         </div>
     );
 }
