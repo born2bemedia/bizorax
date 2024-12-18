11d65c500c7329b5b1c55db1259b05683d08065a
KonstantinRTS <k.developer@acceptance.ltd>
2024-12-18 14:25:11 +0200

    all files 31

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -8,7 +8,7 @@ export default function NotFound() {
                     <h1 className="404__title">404</h1>
                     <h2 className="404__subtitle">Page not found</h2>
                     <p className="404__text">Oh, we can’t seem to find the page you’re looking for. Try going back to the previous page or:</p>
-                    <Link className="404__link">Go to the home page</Link>
+                    <Link href="/" className="404__link">Go to the home page</Link>
                 </div>
             </div>
         </div>
